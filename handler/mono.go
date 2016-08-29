bda158c7fb2866e87f4c12192b7831189da4a809
nirasan <nirasan@gmail.com>
2016-08-29 16:27:58 +0900

    ハンドラーを別パッケージに切り出す

---
+++ handler/mono.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/rand"
+	"net/http"
+)
+
+func Mono(w http.ResponseWriter, r *http.Request) {
+	colors := []color.NRGBA{
+		color.NRGBA{0, 0, 0, 255},
+		color.NRGBA{128, 128, 128, 255},
+		color.NRGBA{255, 255, 255, 255},
+	}
+	img := image.NewPaletted(image.Rect(0, 0, 100, 100), color.Palette{colors[0], colors[1], colors[2]})
+	for x := 0; x < 100; x++ {
+		for y := 0; y < 100; y++ {
+			img.Pix[y*img.Stride+x] = uint8(rand.Intn(3))
+		}
+	}
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
+}
--- main.go
+++ main.go
@@ -1,31 +1,12 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/png"
+	"github.com/nirasan/go-kaleidoscope/handler"
 	"log"
-	"math/rand"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", handle)
+	http.HandleFunc("/mono", handler.Mono)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func handle(w http.ResponseWriter, r *http.Request) {
-	colors := []color.NRGBA{
-		color.NRGBA{0, 0, 0, 255},
-		color.NRGBA{128, 128, 128, 255},
-		color.NRGBA{255, 255, 255, 255},
-	}
-	img := image.NewPaletted(image.Rect(0, 0, 100, 100), color.Palette{colors[0], colors[1], colors[2]})
-	for x := 0; x < 100; x++ {
-		for y := 0; y < 100; y++ {
-			img.Pix[y*img.Stride+x] = uint8(rand.Intn(3))
-		}
-	}
-	w.Header().Set("Content-Type", "image/png")
-	png.Encode(w, img)
 }
