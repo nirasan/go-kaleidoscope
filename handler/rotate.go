9cf4a40c8998163bd22397982cebdf80733b6d39
nirasan <nirasan@gmail.com>
2016-08-30 11:09:54 +0900

    画像の一部を回転させる

---
+++ handler/rotate.go
@@ -0,0 +1,38 @@
+package handler
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math"
+	"net/http"
+)
+
+func Rotate(w http.ResponseWriter, r *http.Request) {
+	colors := []color.NRGBA{
+		color.NRGBA{0, 0, 0, 255},
+		color.NRGBA{128, 128, 128, 255},
+		color.NRGBA{255, 255, 255, 255},
+	}
+	// 縦縞
+	img := image.NewPaletted(image.Rect(0, 0, 100, 100), color.Palette{colors[0], colors[1], colors[2]})
+	for x := 0; x < 100; x++ {
+		for y := 0; y < 100; y++ {
+			img.Pix[y*img.Stride+x] = uint8((x / 5) % 3)
+		}
+	}
+	for x := 50; x < 100; x++ {
+		for y := 50; y < 100; y++ {
+			for _, deg := range []float64{90, -90} {
+				rad := deg * math.Pi / 180
+				x1, y1 := float64(x-50), float64(y-50)
+				x2 := x1*math.Cos(rad) - y1*math.Sin(rad)
+				y2 := x1*math.Sin(rad) + y1*math.Cos(rad)
+				x3, y3 := int(x2+50), int(y2+50)
+				img.Pix[y3*img.Stride+x3] = img.Pix[y*img.Stride+x]
+			}
+		}
+	}
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
+}
--- main.go
+++ main.go
@@ -9,5 +9,6 @@ import (
 func main() {
 	http.HandleFunc("/mono", handler.Mono)
 	http.HandleFunc("/stripe", handler.Stripe)
+	http.HandleFunc("/rotate", handler.Rotate)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
