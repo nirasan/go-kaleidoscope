9b6d0ffa107b80714c31a1ee1db357a34535894f
nirasan <nirasan@gmail.com>
2016-08-30 17:00:49 +0900

    add comment

--- main.go
+++ main.go
@@ -8,15 +8,23 @@ import (
 )
 
 func main() {
+	// 万華鏡画像表示 | クエリパラメータの w で画像サイズ(ピクセル) n で生成要素数(多いほどごちゃっとする)の指定
 	http.HandleFunc("/", handler.Kaleidoscope)
+	// 砂嵐画像表示
 	http.HandleFunc("/mono", handler.Mono)
+	// 縞画像表示
 	http.HandleFunc("/stripe", handler.Stripe)
+	// 縞画像の回転
 	http.HandleFunc("/rotate", handler.Rotate)
+	// 縞画像の扇形に切り抜いて回転してコピー
 	http.HandleFunc("/sector", handler.Sector)
+	// sector でピクセルの欠けが出たので修正版
 	http.HandleFunc("/sector2", handler.Sector2)
+	// 環境変数から公開ポートの指定
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+	// サーバー起動
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
